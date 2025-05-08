412ce56f4f89456db14ccb60f65614baa104c91b
lowskydev <code@lowsky.dev>
2025-05-09 00:22:01 +0200

    add logo and fivicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { Theater } from "lucide-react";
 import ThemeToggle from "./ThemeToggle";
+import logo from "@/assets/logo.png";
 
 function Navbar() {
   return (
-    <nav className="bg-background border-b border-border shadow p-4 sticky top-0 z-50">
+    <nav className="border-b border-border shadow p-4 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <Link to="/" className="flex items-center">
-          <h1 className="text-xl font-bold text-foreground">Fokuso</h1>
+        <Link to="/" className="flex items-center gap-2">
+          <img src={logo} alt="Fokuso Logo" className="h-14 w-14" />
+          <h1 className="text-2xl font-bold text-foreground">Fokuso</h1>
         </Link>
         <div className="space-x-4">
           <Link to="/login">
