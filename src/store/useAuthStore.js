3044dde3faa83dc289b155035fbca89080265502
lowskydev <code@lowsky.dev>
2025-05-13 00:06:53 +0200

    init zustand

---
+++ src/store/useAuthStore.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  token: localStorage.getItem("token") || null,
+  user: null,
+
+  login: (token, user = null) => {
+    localStorage.setItem("token", token);
+    set({ token, user });
+  },
+
+  logout: () => {
+    localStorage.removeItem("token");
+    set({ token: null, user: null });
+  },
+
+  isAuthenticated: () => !!localStorage.getItem("token"),
+}));
+
+export default useAuthStore;
