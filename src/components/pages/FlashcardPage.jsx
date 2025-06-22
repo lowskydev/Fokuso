84849310f6d18103ecbf5a3ada26f6044b4c569d
lowskydev <code@lowsky.dev>
2025-06-22 02:37:52 +0200

    fix linting

--- src/components/pages/FlashcardPage.jsx
+++ src/components/pages/FlashcardPage.jsx
@@ -279,7 +279,6 @@ function FlashcardsPage() {
                   "bg-blue-500",
                   "bg-green-500",
                   "bg-yellow-500",
-
                   "bg-orange-500",
                   "bg-red-500",
                   "bg-purple-500",
